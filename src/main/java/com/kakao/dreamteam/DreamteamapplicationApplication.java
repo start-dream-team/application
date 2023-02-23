1ae212321f637ce516fd19e29c3f0b6c72d2d8c6
jooney <xmun777@naver.com>
2023-02-23 18:00:31 +0900

    feat: project init

---
+++ src/main/java/com/kakao/dreamteam/DreamteamapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.dreamteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamteamapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamteamapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kakao/dreamteam/DreamteamapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.dreamteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamteamapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
